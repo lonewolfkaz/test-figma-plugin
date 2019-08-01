01df0b459f1076c6f5b3b24138da8b6af1f698cc
Anatoly <rook11@list.ru>
2019-08-01 22:29:31 +0300

    master: create-ellipse

--- code.ts
+++ code.ts
@@ -14,14 +14,14 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
+  if (msg.type === 'create-ellipse') {
     const nodes: SceneNode[] = [];
     for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+      const ellipse = figma.createEllipse();
+      ellipse.x = i * 550;
+      ellipse.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 }, opacity: 0.85 }];
+      figma.currentPage.appendChild(ellipse);
+      nodes.push(ellipse);
     }
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
